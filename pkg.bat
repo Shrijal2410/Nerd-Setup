7f96924f438ee5f9858da8a8ef44a6a90e92e8bd
Shrijal <shrijal2410@gmail.com>
2023-08-24 10:15:54 +0530

    Create: 'pkg.bat' Chocolatey script

---
+++ pkg.bat
@@ -0,0 +1,56 @@
+# Autodesk Fusion 360
+choco install -y fusion360
+
+# DaVinci Resolve
+choco install -y davinciresolve
+
+# Creality Slicer
+choco install -y creality-slicer
+
+# Epic Games Launcher
+choco install -y epicgameslauncher
+
+# Git
+choco install -y git
+
+# Google Chrome
+choco install -y googlechrome
+
+# Google Drive File Stream
+choco install -y googledrive-file-stream
+
+# Microsoft Office Home and Student 2021
+choco install -y microsoft-office-homeandstudent2021
+
+# Visual Studio Code
+choco install -y visualstudiocode
+
+# MinGW C++ Compiler
+choco install -y mingw
+
+# Internet Download Manager
+choco install -y internetdownloadmanager
+
+# Python
+choco install -y python
+
+# qBittorrent
+choco install -y qbittorrent
+
+# Revo Uninstaller
+choco install -y revo-uninstaller
+
+# Spotify
+choco install -y spotify
+
+# Steam
+choco install -y steam
+
+# VLC Media Player
+choco install -y vlc
+
+# WinDirStat
+choco install -y windirstat
+
+# Arduino IDE
+choco install -y arduino
